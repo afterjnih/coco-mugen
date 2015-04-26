fb54b739fa3f8b60ec23ba86668fd5b5cadf8f2d
Koji Ota <afterjnih@gmail.com>
2015-04-26 15:53:13 +0900

    change migration files

--- db/migrate/20150311131241_remove_lengths_from_prices.rb
+++ db/migrate/20150311131241_remove_lengths_from_prices.rb
@@ -1,6 +1,6 @@
 class RemoveLengthsFromPrices < ActiveRecord::Migration
   def change
-    remove_column :prices, :length, :float
-    remove_column :prices, :width, :float
+    # remove_column :prices, :length, :float
+    # remove_column :prices, :width, :float
   end
 end
