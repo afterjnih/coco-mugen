563db07dc0176aa3795cda6d8df18fa104503a59
Koji Ota <afterjnih@gmail.com>
2015-04-26 15:49:28 +0900

    change migration files

--- db/migrate/20150109104324_create_prices.rb
+++ db/migrate/20150109104324_create_prices.rb
@@ -1,8 +1,8 @@
 class CreatePrices < ActiveRecord::Migration
   def change
     create_table :prices do |t|
-      # t.float :length,  null: false
-      # t.float :width,   null:false
+      t.float :length,  null: false
+      t.float :width,   null:false
       t.integer :price, null:false
 
       t.timestamps
