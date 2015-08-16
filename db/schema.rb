6d2c957fe30108debc92b52fb790e83c84669223
Koji Ota <afterjnih@gmail.com>
2015-08-16 16:11:55 +0900

    Integrate phone_number and cellphone_number into phone_number

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150719091229) do
+ActiveRecord::Schema.define(version: 20150816070259) do
 
   create_table "contacts", force: true do |t|
     t.text     "name"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 20150719091229) do
     t.float    "width",              null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "cellphone_number"
     t.string   "residence"
     t.string   "mailing_address"
     t.string   "residence_zip_code"
