1ed4c4b34cb0f19ba57df13589217ea36767858b
Koji Ota <afterjnih@gmail.com>
2015-09-02 01:02:41 +0900

    remove unusable @

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -94,43 +94,43 @@ Webサイトからご注文がありました。
 
   def make_contacting_mail_text(contact)
     text = <<-"EOS"
-氏名: #{@contact.name}
-Email: #{@contact.mail}
-問い合せ内容: #{@contact.content}
+氏名: #{contact.name}
+Email: #{contact.mail}
+問い合せ内容: #{contact.content}
     EOS
   end
 
   def make_ordering_mail_text(order)
     text = <<-"EOS"
-氏名: #{@order.last_name} #{@order.first_name}
-シメイ: #{@order.last_name_kana} #{@order.first_name_kana}
-Email: #{@order.mail}
-電話: #{@order.phone_number}
-〒: #{@order.residence_zip_code}
-住所: #{@order.residence}
-送付先〒: #{@order.mailing_zip_code}
-送付先住所: #{@order.mailing_address}
+氏名: #{order.last_name} #{order.first_name}
+シメイ: #{order.last_name_kana} #{order.first_name_kana}
+Email: #{order.mail}
+電話: #{order.phone_number}
+〒: #{order.residence_zip_code}
+住所: #{order.residence}
+送付先〒: #{order.mailing_zip_code}
+送付先住所: #{order.mailing_address}
     EOS
-    if @order.receipt_enabled == true
+    if order.receipt_enabled == true
       text.concat("\n領収書: 要")
     else
       text.concat("\n領収書: 不要")
     end
-    text.concat("\n宛名: #{@order.phone_number}")
-    text.concat("\n但書: #{@order.phone_number}")
-    unless @order.purpose == 'other'
-      text.concat("\n用途: #{@order.purpose}")
+    text.concat("\n宛名: #{order.phone_number}")
+    text.concat("\n但書: #{order.phone_number}")
+    unless order.purpose == 'other'
+      text.concat("\n用途: #{order.purpose}")
     else
-      text.concat("\n用途: #{@order.other_purpose}")
+      text.concat("\n用途: #{order.other_purpose}")
     end
-    text.concat("\n希望日: #{@order.preffered_date}")
-    unless @order.size == 'other'
-      text.concat("\nサイズ: #{@order.size}")
+    text.concat("\n希望日: #{order.preffered_date}")
+    unless order.size == 'other'
+      text.concat("\nサイズ: #{order.size}")
     else
       text.concat("\nサイズ: その他")
-      text.concat("\n縦: #{@order.length}cm")
-      text.concat("\n横: #{@order.width}cm")
+      text.concat("\n縦: #{order.length}cm")
+      text.concat("\n横: #{order.width}cm")
     end
-    text.concat("\n備考: #{@order.remarks}")
+    text.concat("\n備考: #{order.remarks}")
   end
 end
